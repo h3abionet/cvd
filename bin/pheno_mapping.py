1e2f0b150d1c6856cbb37618378a49df6945c9f2
Mamana <mypandos@gmail.com>
2019-09-18 15:57:41 +0200

    Fix error when hardcode format not in mapping file

--- bin/pheno_mapping.py
+++ bin/pheno_mapping.py
@@ -54,76 +54,77 @@ def read_mapping(mapping_file):
 
         ## Process non formula variables first
         for format in option_formats:
-            for item in data_formats[format]:
-                # print(item)
-                item_str = ', '.join([':'.join([key,item[key]]) for key in item if key != None and item[key] != None]) # to use for error logging
-                if 'Study Variable Name' in item and 'Study Variable Format' in item:
-                    ## Get old study variable
-                    study_var = item['Study Variable Name'] #TODO check this is not empty
-                    study_option = item['Study Variable Coding']
-                    var_format = item['Study Variable Format']
-                    if item['Study Variable Format'] != '':
-                        if var_format == 'text': ## Case 1
-                            study_data[study_var] = {}
-                            study_options = study_option
-                        elif var_format == 'single_choice': # Case 2: multiple options field
-                            study_options = {}
-                            # print(study_option)
-                            for it in [it for it in study_option.split("|")]:
-                                newStr = pp.commaSeparatedList.parseString(it).asList() # in case there is a comma in an option
-                                try:
-                                    study_options[newStr[0]] = newStr[1].replace('\'','')
-                                except:
-                                    error_message(4, study_option, item_str)
-                        elif var_format in ['automated', 'hardcode']: ## Case 8: automated
-                            study_data[study_var] = {}
-                            study_options = ''
-                        else:
-                            error_message(6, var_format, item_str, mapping_file)
-                        study_data[study_var] = {'study_options':study_options}
+            if format in data_formats:
+                for item in data_formats[format]:
+                    # print(item)
+                    item_str = ', '.join([':'.join([key,item[key]]) for key in item if key != None and item[key] != None]) # to use for error logging
+                    if 'Study Variable Name' in item and 'Study Variable Format' in item:
+                        ## Get old study variable
+                        study_var = item['Study Variable Name'] #TODO check this is not empty
+                        study_option = item['Study Variable Coding']
+                        var_format = item['Study Variable Format']
+                        if item['Study Variable Format'] != '':
+                            if var_format == 'text': ## Case 1
+                                study_data[study_var] = {}
+                                study_options = study_option
+                            elif var_format == 'single_choice': # Case 2: multiple options field
+                                study_options = {}
+                                # print(study_option)
+                                for it in [it for it in study_option.split("|")]:
+                                    newStr = pp.commaSeparatedList.parseString(it).asList() # in case there is a comma in an option
+                                    try:
+                                        study_options[newStr[0]] = newStr[1].replace('\'','')
+                                    except:
+                                        error_message(4, study_option, item_str)
+                            elif var_format in ['automated', 'hardcode']: ## Case 8: automated
+                                study_data[study_var] = {}
+                                study_options = ''
+                            else:
+                                error_message(6, var_format, item_str, mapping_file)
+                            study_data[study_var] = {'study_options':study_options}
 
-                        ## Get new CVD variable and options
-                        if 'NEW Variable Name' in item:
-                            if item['NEW Variable Name'] != '':
-                                new_var = item['NEW Variable Name']
-                                new_option = item['NEW to Study Mapping']
-                                new_coding = item['NEW Variable Coding']
-                                study_data[study_var]['new_variable'] = new_var
-                                 # in case there is a comma in an option
-                                if var_format == 'text': ## Case 1: text
-                                    new_mapping = new_option
+                            ## Get new CVD variable and options
+                            if 'NEW Variable Name' in item:
+                                if item['NEW Variable Name'] != '':
+                                    new_var = item['NEW Variable Name']
+                                    new_option = item['NEW to Study Mapping']
+                                    new_coding = item['NEW Variable Coding']
+                                    study_data[study_var]['new_variable'] = new_var
+                                     # in case there is a comma in an option
+                                    if var_format == 'text': ## Case 1: text
+                                        new_mapping = new_option
+                                        study_data[study_var]['new_mapping'] = new_mapping
+                                    elif var_format == 'single_choice': # Case 2: multiple options field
+                                        new_mapping = {}
+                                        for option in [it.split('=') for it in new_option.split("|")]:
+                                            option = [opt.strip() for opt in option] # remove whitespace
+                                            key = option[0]
+                                            vals = option[1].split(',')
+                                            if len(vals) == 1:
+                                                new_mapping[vals[0]] = key
+                                            else:
+                                                for val in vals:
+                                                    new_mapping[val.strip()] = key.strip()
+                                    else:
+                                        error_message(6, var_format, item_str, mapping_file)
+                                    # elif var_format == 'automated': ## Case 9: automated
+                                    #     if '+' in new_option:
+                                    #         options = [it.strip() for it in new_option.split('=')[1].replace('\'','').split('+')]
+                                    #     new_mapping = new_option
+                                    # elif new_coding == '': # Case 1: free text field
+                                    #     new_mapping = {}
                                     study_data[study_var]['new_mapping'] = new_mapping
-                                elif var_format == 'single_choice': # Case 2: multiple options field
-                                    new_mapping = {}
-                                    for option in [it.split('=') for it in new_option.split("|")]:
-                                        option = [opt.strip() for opt in option] # remove whitespace
-                                        key = option[0]
-                                        vals = option[1].split(',')
-                                        if len(vals) == 1:
-                                            new_mapping[vals[0]] = key
-                                        else:
-                                            for val in vals:
-                                                new_mapping[val.strip()] = key.strip()
+                                    study_data[study_var]['new_format'] = var_format
                                 else:
-                                    error_message(6, var_format, item_str, mapping_file)
-                                # elif var_format == 'automated': ## Case 9: automated
-                                #     if '+' in new_option:
-                                #         options = [it.strip() for it in new_option.split('=')[1].replace('\'','').split('+')]
-                                #     new_mapping = new_option
-                                # elif new_coding == '': # Case 1: free text field
-                                #     new_mapping = {}
-                                study_data[study_var]['new_mapping'] = new_mapping
-                                study_data[study_var]['new_format'] = var_format
+                                    error_message(1, item_str)
                             else:
                                 error_message(1, item_str)
                         else:
-                            error_message(1, item_str)
+                            error_message(2, item_str)
                     else:
-                        error_message(2, item_str)
-                else:
-                    error_message(3, item_str)
+                        error_message(3, item_str)
 
-    print(study_data)
+    # print(study_data)
     return study_data ## return variables list as well to use during convertion
 
 def pheno_mapping(pheno_file, mapping_file):
@@ -165,7 +166,7 @@ def pheno_mapping(pheno_file, mapping_file):
                                 error_message(5, var, item[var], item_str)
                     else:
                         print(var, var_format)
-                        print('mamana')
+                        # print('mamana')
                         time.sleep(10)
                     # print("-------FINAL",[item])
                     # time.sleep(10)
